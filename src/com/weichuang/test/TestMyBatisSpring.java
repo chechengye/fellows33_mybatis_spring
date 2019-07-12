ecb5a93f64209a322789cccd08160a8b848021b8
chechengye <26292600@qq.com>
2019-07-12 16:25:05 +0800

    Initial commit

---
+++ src/com/weichuang/test/TestMyBatisSpring.java
@@ -0,0 +1,17 @@
+package com.weichuang.test;
+
+import com.weichuang.dao.UserMapper;
+import com.weichuang.pojo.User;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestMyBatisSpring {
+    @Test
+    public void testFn(){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        UserMapper userMapper = (UserMapper)applicationContext.getBean("userMapper");
+        User user = userMapper.getUserById(4);
+        System.out.println(user);
+    }
+}
